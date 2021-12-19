c709ca2d3ce7ce486102b6c1f132803533eacba6
Sushant Karn <95271146+HubspotElement@users.noreply.github.com>
2021-12-19 18:16:15 +0545

    cool-text.js

---
+++ cool-text.js
@@ -0,0 +1,262 @@
+var _0x7a0d = [
+    'a b c d e f g h i j k l m n o p q r s t u v w x y z ',
+    'ᗩ ᗷ ᑕ ᗪ E ᖴ G ᕼ I ᒍ K ᒪ ᗰ ᑎ O ᑭ ᑫ ᖇ ᔕ T ᑌ ᐯ ᗯ \u166D Y ᘔ ',
+    '[̲̅a̲̅] [̲̅b̲̅] [̲̅c̲̅] [̲̅d̲̅] [̲̅e̲̅] [̲̅f̲̅] [̲̅g̲̅] [̲̅h̲̅] [̲̅i̲̅] [̲̅j̲̅] [̲̅k̲̅] [̲̅l̲̅] [̲̅m̲̅] [̲̅n̲̅] [̲̅o̲̅] [̲̅p̲̅] [̲̅q̲̅] [̲̅r̲̅] [̲̅s̲̅] [̲̅t̲̅] [̲̅u̲̅] [̲̅v̲̅] [̲̅w̲̅] [̲̅x̲̅] [̲̅y̲̅] [̲̅z̲̅] ',
+    '\u24D0 \u24D1 \u24D2 \u24D3 \u24D4 \u24D5 \u24D6 \u24D7 \u24D8 \u24D9 \u24DA \u24DB \u24DC \u24DD \u24DE \u24DF \u24E0 \u24E1 \u24E2 \u24E3 \u24E4 \u24E5 \u24E6 \u24E7 \u24E8 \u24E9 ',
+    'α в \xA2 \u2202 є f g н ι נ к ℓ м и σ ρ q я ѕ т υ ν ω χ у z ',
+    '͏a͏ b͏ c͏ d͏ e͏ f͏ g͏ h͏ i͏ j͏ k͏ l͏ m͏ n͏ o͏ p͏ q͏ r͏ s͏ t͏ u͏ v͏ w͏ x͏ y͏ z͏ ',
+    '\u20B3 \u0E3F \u20B5 Đ Ɇ \u20A3 \u20B2 Ⱨ ł J \u20AD Ⱡ \u20A5 \u20A6 Ø \u20B1 Q Ɽ \u20B4 \u20AE Ʉ V \u20A9 Ӿ Ɏ Ⱬ ',
+    '۰۪۫a۪۫۰ ۰۪۫b۪۫۰ ۰۪۫c۪۫۰ ۰۪۫d۪۫۰ ۰۪۫e۪۫۰ ۰۪۫f۪۫۰ ۰۪۫g۪۫۰ ۰۪۫h۪۫۰ ۰۪۫i۪۫۰ ۰۪۫j۪۫۰ ۰۪۫k۪۫۰ ۰۪۫l۪۫۰ ۰۪۫m۪۫۰ ۰۪۫n۪۫۰ ۰۪۫o۪۫۰ ۰۪۫p۪۫۰ ۰۪۫q۪۫۰ ۰۪۫r۪۫۰ ۰۪۫s۪۫۰ ۰۪۫t۪۫۰ ۰۪۫u۪۫۰ ۰۪۫v۪۫۰ ۰۪۫w۪۫۰ ۰۪۫x۪۫۰ ۰۪۫y۪۫۰ ۰۪۫z۪۫۰ ',
+    'ä ḅ ċ ď ệ ḟ ġ ḧ ï j ḳ ŀ ṃ ń ö ṗ q ŕ ṩ ẗ ü ṿ ẅ ẍ ÿ ẓ ',
+    'Ꮧ Ᏸ ፈ Ꮄ Ꮛ Ꭶ Ꮆ Ꮒ Ꭵ Ꮰ Ꮶ Ꮭ Ꮇ Ꮑ Ꭷ Ꭾ Ꭴ Ꮢ Ꮥ Ꮦ Ꮼ Ꮙ Ꮗ ጀ Ꭹ ፚ ',
+    'α β c δ ε ζ g h ι j κ l m π ο ρ ς r s τ υ v ω χ ψ z ',
+    'Ḁ ḃ ḉ Ḋ ḕ ḟ Ḡ ḧ ḭ j Ḳ Ḷ ṁ Ṇ ṏ Ṗ q ṙ Ṡ Ṯ ṳ Ṽ ẇ Ẍ ẏ Ẓ ',
+    'Ꭺ b Ꮯ Ꭰ Ꭼ f Ꮆ h Ꭵ j Ꮶ Ꮮ m Ꮑ Ꮎ Ꮲ q Ꮢ s Ꮖ u Ꮙ Ꮃ x Ꮍ Ꮓ ',
+    'α в c d є f g h í j k l m n σ p q r s t u v w х ч z ',
+    'ǟ ɮ ƈ ɖ ɛ ʄ ɢ ɦ ɨ ʝ ӄ ʟ ʍ ռ օ ք զ ʀ ֆ ȶ ʊ ʋ ա Ӽ ʏ ʐ ',
+    '̾a̾ b̾ c̾ d̾ e̾ f̾ g̾ h̾ i̾ j̾ k̾ l̾ m̾ n̾ o̾ p̾ q̾ r̾ s̾ t̾ u̾ v̾ w̾ x̾ y̾ z̾ ',
+    'ａ ｂ ｃ ｄ ｅ ｆ ｇ ｈ ｉ ｊ ｋ ｌ ｍ ｎ ｏ ｐ ｑ ｒ ｓ ｔ ｕ ｖ ｗ ｘ ｙ ｚ ',
+    'ᴀ ʙ ᴄ ᴅ ᴇ ᴈ ɢ ʜ ɪ ᴊ ᴋ ʟ ᴍ ɴ ᴏ ᴘ ᴓ ʀ ᴤ ᴛ ᴜ ᴠ ᴡ ᴥ ʏ ᴢ ',
+    'ɐ q ɔ p ǝ ɟ ƃ ɥ ı ɾ ʞ l ɯ u o d b ɹ s ʇ n ʌ ʍ x ʎ z ',
+    'ܣaܣ ܣbܣ ܣcܣ ܣdܣ ܣeܣ ܣfܣ ܣgܣ ܣhܣ ܣiܣ ܣjܣ ܣkܣ ܣlܣ ܣmܣ ܣnܣ ܣoܣ ܣpܣ ܣqܣ ܣrܣ ܣsܣ ܣtܣ ܣuܣ ܣvܣ ܣwܣ ܣxܣ ܣyܣ ܣzܣ ',
+    '\u2665a\u2665 \u2665b\u2665 \u2665c\u2665 \u2665d\u2665 \u2665e\u2665 \u2665f\u2665 \u2665g\u2665 \u2665h\u2665 \u2665i\u2665 \u2665j\u2665 \u2665k\u2665 \u2665l\u2665 \u2665m\u2665 \u2665n\u2665 \u2665o\u2665 \u2665p\u2665 \u2665q\u2665 \u2665r\u2665 \u2665s\u2665 \u2665t\u2665 \u2665u\u2665 \u2665v\u2665 \u2665w\u2665 \u2665x\u2665 \u2665y\u2665 \u2665z\u2665 ',
+    '̶a̶ ̶b̶ ̶c̶ ̶d̶ ̶e̶ ̶f̶ ̶g̶ ̶h̶ ̶i̶ ̶j̶ ̶k̶ ̶l̶ ̶m̶ ̶n̶ ̶o̶ ̶p̶ ̶q̶ ̶r̶ ̶s̶ ̶t̶ ̶u̶ ̶v̶ ̶w̶ ̶x̶ ̶y̶ ̶z̶',
+    'length',
+    'toLowerCase',
+    'val',
+    '#text',
+    '',
+    'special',
+    'charAt',
+    'g',
+    'replace',
+    'boxed',
+    '[̲̅',
+    '̲̅]',
+    'blurry',
+    '͏',
+    'diamond',
+    '۰۪۫',
+    '۪۫۰',
+    'stinky',
+    '̾',
+    'upsideDown',
+    'join',
+    'reverse',
+    'split',
+    'love1',
+    'ܣ',
+    'love2',
+    '\u2665',
+    'Strikethrough',
+    '̶',
+    'bubble',
+    'antrophobia',
+    'currency',
+    'dirty',
+    'fairy',
+    'greek_style',
+    'knight',
+    'magic',
+    'paranormal',
+    'sorcerer',
+    'thin',
+    'tiny',
+    'substring',
+    '#',
+    'selectedText',
+    'removeClass',
+    ':text',
+    'flash',
+    'getElementById',
+    'jsFunctionName',
+    'seninputId',
+    '.fontPreview',
+    'addClass',
+    '.',
+    'Text',
+    'setData',
+    'clipboardData',
+    'Your text here..',
+    'value',
+    'defaultValue',
+    'focus',
+    '.defaulttext',
+    'blur'
+];
+function generateStylishText()
+    /*Scope Closed:false | writes:true*/
+    {
+        var _0xcf2dx2 = 9;
+        var _0xcf2dx3 = 'a b c d e f g h i j k l m n o p q r s t u v w x y z ';
+        var _0xcf2dx4 = [];;
+        var _0xcf2dx5 = 25;
+        var _0xcf2dx6 = _0xcf2dx4.length;
+        var _0xcf2dx7 = $('#text').val().toLowerCase();
+        var _0xcf2dx8 = [];;
+        var _0xcf2dx9 = [];;
+        var _0xcf2dxa = [];;
+        for (k = 1; k <= _0xcf2dx6; k++) {
+            _0xcf2dx8[k] = _0xcf2dx7;
+            var _0xcf2dxb = _0x7a0d[26];
+            for (i = 0; i <= _0xcf2dx5; i++) {
+                if (k == 1) {
+                    _0xcf2dxa[k] = _0x7a0d[27];
+                    _0xcf2dx8[k] = _0xcf2dx8[k][_0x7a0d[30]](new RegExp(_0xcf2dx3[_0x7a0d[28]](i * 2), _0x7a0d[29]), _0xcf2dx4[k - 1][_0x7a0d[28]](i * 2));
+                } else {
+                    if (k == 2) {
+                        _0xcf2dxa[k] = _0x7a0d[31];
+                        _0xcf2dx8[k] = _0xcf2dx8[k][_0x7a0d[30]](new RegExp(_0xcf2dx3[_0x7a0d[28]](i * 2), _0x7a0d[29]), _0x7a0d[32] + _0xcf2dx4[k - 1][_0x7a0d[28]](i * 8 + 3) + _0x7a0d[33]);
+                    } else {
+                        if (k == 5) {
+                            _0xcf2dxa[k] = _0x7a0d[34];
+                            _0xcf2dx8[k] = _0xcf2dx8[k][_0x7a0d[30]](new RegExp(_0xcf2dx3[_0x7a0d[28]](i * 2), _0x7a0d[29]), _0x7a0d[35] + _0xcf2dx4[k - 1][_0x7a0d[28]](i * 3 + 1));
+                        } else {
+                            if (k == 7) {
+                                _0xcf2dxa[k] = _0x7a0d[36];
+                                _0xcf2dx8[k] = _0xcf2dx8[k][_0x7a0d[30]](new RegExp(_0xcf2dx3[_0x7a0d[28]](i * 2), _0x7a0d[29]), _0x7a0d[37] + _0xcf2dx4[k - 1][_0x7a0d[28]](i * 8 + 3) + _0x7a0d[38]);
+                            } else {
+                                if (k == 15) {
+                                    _0xcf2dxa[k] = _0x7a0d[39];
+                                    _0xcf2dx8[k] = _0xcf2dx8[k][_0x7a0d[30]](new RegExp(_0xcf2dx3[_0x7a0d[28]](i * 2), _0x7a0d[29]), _0x7a0d[40] + _0xcf2dx4[k - 1][_0x7a0d[28]](i * 3 + 1));
+                                } else {
+                                    if (k == 18) {
+                                        _0xcf2dxa[k] = _0x7a0d[41];
+                                        _0xcf2dx8[k] = _0xcf2dx8[k][_0x7a0d[30]](new RegExp(_0xcf2dx3[_0x7a0d[28]](i * 2), _0x7a0d[29]), _0xcf2dx4[k - 1][_0x7a0d[28]](i * 2)[_0x7a0d[44]](_0x7a0d[26])[_0x7a0d[43]]()[_0x7a0d[42]](_0x7a0d[26]));
+                                    } else {
+                                        if (k == 19) {
+                                            _0xcf2dxa[k] = _0x7a0d[45];
+                                            _0xcf2dx8[k] = _0xcf2dx8[k][_0x7a0d[30]](new RegExp(_0xcf2dx3[_0x7a0d[28]](i * 2), _0x7a0d[29]), _0x7a0d[46] + _0xcf2dx4[k - 1][_0x7a0d[28]](i * 4 + 1));
+                                        } else {
+                                            if (k == 20) {
+                                                _0xcf2dxa[k] = _0x7a0d[47];
+                                                _0xcf2dx8[k] = _0xcf2dx8[k][_0x7a0d[30]](new RegExp(_0xcf2dx3[_0x7a0d[28]](i * 2), _0x7a0d[29]), _0x7a0d[48] + _0xcf2dx4[k - 1][_0x7a0d[28]](i * 4 + 1));
+                                            } else {
+                                                if (k == 21) {
+                                                    _0xcf2dxa[k] = _0x7a0d[49];
+                                                    _0xcf2dx8[k] = _0xcf2dx8[k][_0x7a0d[30]](new RegExp(_0xcf2dx3[_0x7a0d[28]](i * 2), _0x7a0d[29]), _0x7a0d[50] + _0xcf2dx4[k - 1][_0x7a0d[28]](i * 4 + 1));
+                                                } else {
+                                                    _0xcf2dx8[k] = _0xcf2dx8[k][_0x7a0d[30]](new RegExp(_0xcf2dx3[_0x7a0d[28]](i * 2), _0x7a0d[29]), _0xcf2dx4[k - 1][_0x7a0d[28]](i * 2));
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                ;
+                if (k == 3) {
+                    _0xcf2dxa[k] = _0x7a0d[51];
+                }
+                ;
+                if (k == 4) {
+                    _0xcf2dxa[k] = _0x7a0d[52];
+                }
+                ;
+                if (k == 5) {
+                    _0xcf2dxa[k] = _0x7a0d[34];
+                }
+                ;
+                if (k == 6) {
+                    _0xcf2dxa[k] = _0x7a0d[53];
+                }
+                ;
+                if (k == 8) {
+                    _0xcf2dxa[k] = _0x7a0d[54];
+                }
+                ;
+                if (k == 9) {
+                    _0xcf2dxa[k] = _0x7a0d[55];
+                }
+                ;
+                if (k == 10) {
+                    _0xcf2dxa[k] = _0x7a0d[56];
+                }
+                ;
+                if (k == 11) {
+                    _0xcf2dxa[k] = _0x7a0d[57];
+                }
+                ;
+                if (k == 12) {
+                    _0xcf2dxa[k] = _0x7a0d[58];
+                }
+                ;
+                if (k == 13) {
+                    _0xcf2dxa[k] = _0x7a0d[59];
+                }
+                ;
+                if (k == 14) {
+                    _0xcf2dxa[k] = _0x7a0d[60];
+                }
+                ;
+                if (k == 16) {
+                    _0xcf2dxa[k] = _0x7a0d[61];
+                }
+                ;
+                if (k == 17) {
+                    _0xcf2dxa[k] = _0x7a0d[62];
+                }
+            }
+            ;
+            if (k == 18) {
+                var _0xcf2dxc = _0xcf2dx8[k][_0x7a0d[22]];
+                while (_0xcf2dxc > 0) {
+                    _0xcf2dxb += _0xcf2dx8[k][_0x7a0d[63]](_0xcf2dxc - 1, _0xcf2dxc);
+                    _0xcf2dxc--;
+                }
+                ;
+                _0xcf2dx8[k] = _0xcf2dxb;
+            }
+            ;
+            $(_0x7a0d[64] + _0xcf2dxa[k])[_0x7a0d[24]](_0xcf2dx8[k]);
+            try {
+                $(_0x7a0d[67])[_0x7a0d[66]](_0x7a0d[65]);
+                var _0xcf2dxd = _0xcf2dx8[k];
+                var _0xcf2dxe = _0x7a0d[68] + k;
+                var _0xcf2dxf = document(_0xcf2dxe);
+                _0xcf2dxf[_0x7a0d[70]](_0xcf2dxd);
+                _0xcf2dxf[_0x7a0d[71]](_0xcf2dxa[k]);
+            } 
+        }
+        ;
+        return false;
+    }
+function copyTextForIe(_0xcf2dx11)
+    /*Scope Closed:false | writes:false*/
+    {
+        $('.fontPreview').removeClass('selectedText');
+        $('.' + _0xcf2dx11).addClass('selectedText');
+        var _0xcf2dx12 = $('#' + _0xcf2dx11).val();
+        window.clipboardData.setData('Text', _0xcf2dx12);
+        return false;
+    }
+function selectText(_0xcf2dx14)
+    /*Scope Closed:false | writes:false*/
+    {
+        $('.fontPreview').removeClass('selectedText');
+        $('.' + _0xcf2dx14).addClass('selectedText');
+    }
+function contentChange()
+    /*Scope Closed:false | writes:false*/
+    {
+        var _0xcf2dx16 = $('#text').val();
+        if (_0xcf2dx16 == 'Your text here..') {
+            $('#text').val('');
+            $('.defaulttext').focus(function ()
+                /* Called:undefined | Scope Closed:false| writes:true*/
+                {
+                    if (this.value == this.defaultValue) {
+                        this.value = '';
+                    }
+                });
+            $('.defaulttext').blur(function ()
+                /* Called:undefined | Scope Closed:false| writes:true*/
+                {
+                    if (this.value == '') {
+                        this.value = this.defaultValue;
+                    }
+                });
+        }
+    }
